0a74f934e2da633199a5c57e486f3bb1ca6d7b46
davinellulinvega <casparluc@gmail.com>
2013-05-27 19:34:32 +0200

    "Define StudentController methods"

--- studentcontroller.h
+++ studentcontroller.h
@@ -1,10 +1,24 @@
 #ifndef STUDENTCONTROLLER_H
 #define STUDENTCONTROLLER_H
 
+#include "student.h"
+#include <vector>
+
 class StudentController
 {
 public:
+    //Constructor
     StudentController();
+
+    //Methods
+    bool add(Student student);
+    bool update(Student student);
+    bool remove(id);
+    Student query(id);
+    vector<Student> queryAll();
+
+    //Static Methods
+    static bool isValidId(id);
 };
 
 #endif // STUDENTCONTROLLER_H
