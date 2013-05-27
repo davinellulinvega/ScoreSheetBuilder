d5b714aa2233577db0837f7ba6f61017cab8f827
davinellulinvega <casparluc@gmail.com>
2013-05-27 18:48:54 +0200

    "Implement Getter and setter for Score"

--- score.cpp
+++ score.cpp
@@ -1,5 +1,24 @@
 #include "score.h"
 
+//Constructor
 Score::Score()
 {
+}
+
+//Getter & Setter
+//Id
+int Score::getId() {
+    //Retrun the id
+    return this->m_id;
+}
+
+//Mark
+char Score::getMark() {
+    //Return the mark
+    return this->m_mark;
+}
+
+void Score::setMark(mark) {
+    //Set the mark
+    this->m_mark=mark;
 }
