8d558a93d355205e0636f8b5beab997a1d2c3eda
davinellulinvega <casparluc@gmail.com>
2013-05-27 19:46:10 +0200

    "Define ParametersController methods"

--- parameterscontroller.h
+++ parameterscontroller.h
@@ -1,10 +1,19 @@
 #ifndef PARAMETERSCONTROLLER_H
 #define PARAMETERSCONTROLLER_H
 
+#include "parameters.h"
+
 class ParametersController
 {
 public:
+    //Constructor
     ParametersController();
+
+    //Methods
+    bool add(Parameters parameters);
+    bool update(Parameters parameters);
+    bool remove(Parameters parameters);
+    Parameters query();
 };
 
 #endif // PARAMETERSCONTROLLER_H
