bec3711ab215780972558a9ebd978d19f6e008fb
davinellulinvega <casparluc@gmail.com>
2013-05-27 19:43:21 +0200

    "Define PeriodController methods"

--- periodcontroller.h
+++ periodcontroller.h
@@ -1,10 +1,23 @@
 #ifndef PERIODCONTROLLER_H
 #define PERIODCONTROLLER_H
 
+#include "period.h"
+#include <vector>
+
 class PeriodController
 {
 public:
+    //Constructor
     PeriodController();
+
+    //Methods
+    bool add(Period period);
+    bool update(Period period);
+    bool remove(id);
+    Period query(id);
+    vector<Period> queryAll();
+
+    //Static methods
 };
 
 #endif // PERIODCONTROLLER_H
