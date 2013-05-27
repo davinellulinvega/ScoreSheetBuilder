fd07a3f66bffb43a122fa1effb044116be1b65e4
davinellulinvega <casparluc@gmail.com>
2013-05-27 18:23:01 +0200

    "Implement Getter and setter for Lecture"

--- lecture.cpp
+++ lecture.cpp
@@ -1,7 +1,26 @@
 #include "lecture.h"
 
-//Packages
-
+//Constructor
 Lecture::Lecture()
 {
+}
+
+//Getter & Setter
+//Id
+int Lecture::getId() {
+    //Return the id
+    return this->m_id;
+}
+
+//Title
+string Lecture::getTitle() {
+    //Return the title
+    return this->m_title;
+}
+
+void Lecture::setTitle(title) {
+    //Check if the new title is valid
+    if(!title.empty()) {
+        this->m_title=title;
+    }
 }
