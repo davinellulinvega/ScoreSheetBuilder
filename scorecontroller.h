0c57597791d88ff5810f3e21f493c6a520459226
davinellulinvega <casparluc@gmail.com>
2013-05-27 19:37:05 +0200

    "Define ScoreController methods"

--- scorecontroller.h
+++ scorecontroller.h
@@ -1,10 +1,23 @@
 #ifndef SCORECONTROLLER_H
 #define SCORECONTROLLER_H
 
+#include "score.h"
+#include <vector>
+
 class ScoreController
 {
 public:
+    //Constructor
     ScoreController();
+
+    //Methods
+    bool add(Score score);
+    bool update(Score score);
+    bool remove(id);
+    Score query(id);
+    vector<Score> queryAll();
+
+    //Static methods
 };
 
 #endif // SCORECONTROLLER_H
