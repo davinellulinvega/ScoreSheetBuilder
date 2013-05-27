f07e592c824b31524f89c4b2065aec8950980170
davinellulinvega <casparluc@gmail.com>
2013-05-27 19:48:12 +0200

    "Define LectureController methods"

--- lecturecontroller.h
+++ lecturecontroller.h
@@ -1,10 +1,23 @@
 #ifndef LECTURECONTROLLER_H
 #define LECTURECONTROLLER_H
 
+#include "lecture.h"
+#include <vector>
+
 class LectureController
 {
 public:
+    //Constructor
     LectureController();
+
+    //Methods
+    bool add(Lecture lecture);
+    bool update(Lecture lecture);
+    bool remove(id);
+    Lecture query(id);
+    vector<Lecture> queryAll();
+
+    //Static methods
 };
 
 #endif // LECTURECONTROLLER_H
