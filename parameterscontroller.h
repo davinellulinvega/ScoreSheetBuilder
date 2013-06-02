7253f5abade639489bb0e15cdb54161856fe6a67
davinellulinvega <casparluc@gmail.com>
2013-06-02 21:17:33 +0200

    "Add members to ParameterController"

--- parameterscontroller.h
+++ parameterscontroller.h
@@ -1,6 +1,7 @@
 #ifndef PARAMETERSCONTROLLER_H
 #define PARAMETERSCONTROLLER_H
 
+#include <QtXml>
 #include "parameters.h"
 
 class ParametersController
@@ -14,6 +15,10 @@ public:
     bool update(Parameters parameters);
     bool remove(Parameters parameters);
     Parameters query();
+
+private:
+    QDomNode m_parametersNode;
+    QDomNodeList m_parameters
 };
 
 #endif // PARAMETERSCONTROLLER_H
